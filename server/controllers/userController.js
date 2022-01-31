4635659458b5fd9913c0997aeb2cc3ebf3c1e446
Jackson Tong <jacksonktong@gmail.com>
2022-01-31 10:26:13 -0800

    updated some syntax
    Co-authored-by: nico flores

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 const models = require('../models/Model.js')
 const CryptoJS = require("crypto-js");
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const userController = {};
 
